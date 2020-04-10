b336afc74e3411c03f57d7afdff5579a943d7088
Alejandro <836445882@qq.com>
2020-04-10 18:36:16 +0800

    模块化 store 删除了 models

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,40 +1,12 @@
 
-import recordListModel from '@/models/recordListModel.ts';
-import tagListModel from '@/models/tagListModel.ts';
+import recordStore from '@/store/recordStore.ts';
+import tagStore from '@/store/tagStore.ts';
 
 const store = {
-
-    // record store
-    recordList: recordListModel.fetch(),
-    createRecord: (record: RecordItem) => recordListModel.create(record),
-
+    ...recordStore,
+    ...tagStore
     // 1、全局变量太多
-    // 2、严重依赖 window
-
-    // tag store
-    tagList: tagListModel.fetch(),
-
-    findTag: (id: string) => {
-        return window.store.tagList.filter((t) => t.id === id)[0];
-    },
-
-    createTag: (name: string) => {
-        const message = tagListModel.create(name);
-        if (message === "duplicated") {
-            window.alert("标签名重复");
-        } else if (message === 'success') {
-            window.alert('添加成功')
-        }
-    },
-
-    removeTag: (id: string) => {
-        return tagListModel.remove(id)
-    },
-
-    updateTag: (id: string, name: string) => {
-        return tagListModel.update(id, name)
-
-    }
+    // 2、严重依赖 window   
 };
 
 export default store

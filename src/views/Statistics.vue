337b85dc29410031c0c736b1d6facaaf5ea4302e
Alejandro <836445882@qq.com>
2020-04-15 17:37:39 +0800

    抽离 Tabs.vue 使用在Statistics.vue 里面

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,34 +1,65 @@
 <template>
-<div> 
-    <Layout>
-        <p>Statistics.vue</p>
-    </Layout>
-</div> 
+  <Layout>
+    <Tabs class-prefix="type" :data-source="typeList" :value.sync="type" />
+    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval" />
+    <div>
+        type:{{type}}
+        <br/>
+        interval:{{interval}}
+    </div>
+  </Layout>
 </template>
 
 <script>
-    export default {
-        name: 'Statistics',
-        
-    }
+import Types from "@/components/Money/Types.vue";
+import Tabs from "@/components/Tabs.vue";
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component({
+  components: { Types, Tabs }
+})
+export default class Statistics extends Vue {
+  type = "-";
+  interval = "day";
+  intervalList = [
+    { text: "按天", value: "day" },
+    { text: "按周", value: "week" },
+    { text: "按月", value: "month" },
+  ];
+  typeList = [
+    { text: "支出", value: "-" },
+    { text: "收入", value: "+" },
+  ];
+}
 </script>
 
 <style lang="scss" scoped>
-     .nav-wrapper{
-        display: flex;
-        // border: 1px solid green;
-        flex-direction: column;
-        // min-height: 100vh;//最小高度100vh，也就是沾满可视高度
-        // 考虑如果内容太多导航栏会出现在更下面，所以固定高度更合适height: 100vh；
-        height: 100vh;
+::v-deep .type-tabs-item {
+  background: white;
+  &.selected {
+    background: #C4C4C4;
+    &::after {
+      display: none;
     }
-    .content{
-        // border: 1px solid blue;
-        overflow:auto;
-        //内容太多的话同样会让导航栏看不见，
-        //  overflow:auto;让内容可以滚动，展示滚动条。
-        // 就是蓝色边框里面的内容可以滚动；
-        flex-grow: 1;
-        // 尽量把高度都给content
-    }
+  }
+}
+
+.nav-wrapper {
+  display: flex;
+  // border: 1px solid green;
+  flex-direction: column;
+  // min-height: 100vh;//最小高度100vh，也就是沾满可视高度
+  // 考虑如果内容太多导航栏会出现在更下面，所以固定高度更合适height: 100vh；
+  height: 100vh;
+}
+.content {
+  // border: 1px solid blue;
+  overflow: auto;
+  //内容太多的话同样会让导航栏看不见，
+  //  overflow:auto;让内容可以滚动，展示滚动条。
+  // 就是蓝色边框里面的内容可以滚动；
+  flex-grow: 1;
+  // 尽量把高度都给content
+}
 </style>
